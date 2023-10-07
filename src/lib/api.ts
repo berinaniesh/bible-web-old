96e828b3cce23978cb8c7cec95b5a567d211654d
Berin Aniesh <berinaniesh@gmail.com>
2023-10-08 02:04:53 +0530

    update ui

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,1 +1,1 @@
-export const base = 'https://api.bible.berinaniesh.xyz';
+export const base = 'http://127.0.0.1:7000';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,14 +12,14 @@
       <div class="grid grid-cols-2">
         {#each data.translations as translation}
           <a
-            class="block card card-hover p-4 w-11/12 bg-primary-200 hover:bg-primary-500"
+            class="block card card-hover w-48 h-48 m-2 p-4 w-11/12 bg-primary-200 hover:bg-primary-500"
             href="/{translation.name}"
           >
-            <header class="card-header">{translation.name}</header>
+            <header class="font-bold card-header">{translation.name}</header>
             <section class="p-4">
-              <p>{translation.language}</p>
+              <p>{translation.language}</p><br>
+              <p>{translation.year}</p>
             </section>
-            <footer>Year: {translation.year}</footer>
           </a>
         {/each}
       </div>
