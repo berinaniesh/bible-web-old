212b24a24997258ee160779fb85f9d8b934ece5c
Berin Aniesh <berinaniesh@gmail.com>
2023-12-30 12:18:05 +0530

    add telegram group to about page and include changelog

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,7 +2,7 @@
   <title>About • Bible</title>
 </svelte:head>
 
-<div class="container w-full h-full leading-9 max-w-screen-md mx-auto justify-center items-center py-36 px-8">
+<div class="container w-full h-full leading-9 max-w-screen-md mx-auto justify-center items-center py-8 px-8">
   <h1 class="font-bold text-xl py-4">About</h1>
 
   <ul class="list-disc pl-8">
@@ -27,6 +27,29 @@
         href="https://github.com/berinaniesh/bible-web/issues">github</a
       >.
     </li>
+    <li>
+      To participate in the development, join the <a href="https://t.me/bibleappdesign"><u>telegram group</u></a>. 
+    </li>
+    <li>
+      I make no guarantees of availability, quality or correctness of the API or the web app.
+    </li>
+  </ul>
+</div>
+<div class="container w-full h-full leading-9 max-w-screen-md mx-auto justify-center items-center py-8 px-8">
+  <h1 class="font-bold text-xl py-4">Updates / Changelog</h1>
+
+  <ul class="list-disc pl-8">
+    <li>Sep 9, 2023 - Started working on Bible API</li>
+    <li>Sep 13, 2023 - Feed database with bible translations</li>
+    <li>Sep 15, 2023 - API functional</li>
+    <li>Sep 18, 2023 - Web UI functional</li>
+    <li>Sep 20, 2023 - Add swagger docs to API</li>
+    <li>Nov 2, 2023 - Add next / previous endpoints to API</li>
+    <li>Nov 5, 2023 - Add next / previous buttons to the UI</li>
+    <li>Nov 16, 2023 - Add sitemap / robots.txt</li>
+    <li>Nov 20, 2023 - Add google analytics</li>
+    <li>Dec 21, 2023 - Create Telegram group</li>
+    <li>Dec 30, 2023 - Add changelog to about page</li>
   </ul>
 </div>
 <div class="w-full mx-auto max-w-screen-md container flex justify-center items-center">
